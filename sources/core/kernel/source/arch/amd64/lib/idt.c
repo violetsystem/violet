9f7462db3c76668106da1ef9268a090757cbc828
Keep-Konect <66421869+Keep-Konect@users.noreply.github.com>
2023-08-10 08:08:26 +0200

    Update makefile for global folder

--- sources/core/kernel/source/arch/amd64/lib/idt.c
+++ sources/core/kernel/source/arch/amd64/lib/idt.c
@@ -1,7 +1,7 @@
 #include <arch/amd64/lib/idt.h>
 #include <arch/amd64/lib/gdt.h>
 
-// extern uintptr_t __interrupt_vector[256];
+extern void* __interrupt_vector[256];
 
 static __attribute__((aligned(4096))) idt_t _idt = {};
 
@@ -10,21 +10,21 @@ static idtr_t _idtr = {
     .base = (uint64_t) &_idt,
 };
 
-idt_entry_t idt_entry(uintptr_t handler, uint8_t ist, uint8_t idt_flags) {
+idt_entry_t idt_entry(void* handler, uint8_t ist, uint8_t idt_flags) {
     return (idt_entry_t) {
-        .offset_low = (handler),
+        .offset_low = ((uint64_t)handler) & 0xffff,
         .code_segment = GDT_KERNEL_CODE * 8,
         .ist = ist,
         .attributes = idt_flags,
-        .offset_middle = (handler >> 16),
-        .offset_high = (handler >> 32),
+        .offset_middle = ((uint64_t)handler >> 16) & 0xffff,
+        .offset_high = ((uint64_t)handler >> 32) & 0xffffffff,
         .zero = 0,
     };
 }
 
 void idt_init(void) {
-    // for (uint8_t i = 0; i < 256; i++) {
-    //     _idt.entries[i] = idt_entry(__interrupt_vector[i], 0, IDT_GATE);
-    // }
-    // idt_update(&_idtr);
+    for (uint16_t i = 0; i < 256; i++) {
+        _idt.entries[i] = idt_entry(__interrupt_vector[i], 0, IDT_GATE);
+    }
+    idt_update(&_idtr);
 }
